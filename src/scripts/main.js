89fdedb58f0f03c946329ad429eb0a0184cf66ee
an-marryKyslenko <olih.a@icloud.com>
2025-01-02 22:45:12 +0100

    fix form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
+
 const page = document.documentElement;
 const iconMenu = document.querySelector('.icon--menu');
 const iconClose = document.querySelector('.icon--close');
 
-iconMenu.addEventListener('click', ()=>{
+iconMenu.addEventListener('click', () => {
   page.style.overflow = 'hidden';
 });
 
-iconClose.addEventListener('click', ()=>{
+iconClose.addEventListener('click', () => {
   page.style.overflow = 'visible';
-})
+});
