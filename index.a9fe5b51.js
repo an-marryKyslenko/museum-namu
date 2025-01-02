981e93b0f20ae4fc3a0c6f617aa494584472bd15
an-marryKyslenko <olih.a@icloud.com>
2025-01-02 22:47:44 +0100

    make build

---
+++ index.a9fe5b51.js
@@ -0,0 +1,2 @@
+var e=document.documentElement,n=document.querySelector(".icon--menu"),c=document.querySelector(".icon--close");n.addEventListener("click",function(){e.style.overflow="hidden"}),c.addEventListener("click",function(){e.style.overflow="visible"});
+//# sourceMappingURL=index.a9fe5b51.js.map
